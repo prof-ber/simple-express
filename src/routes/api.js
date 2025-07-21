7d43e69dec8fa4d4b26d35a4d87c3bf46f38d279
Ber <mauvemagician@gmail.com>
2025-07-21 14:52:41 -0300

    Fix api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Endpoint GET
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 // Endpoint POST
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Data received');
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Data received");
 });
 
 module.exports = router;
